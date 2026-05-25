479c03db387ac8ceb9c1def72c22efb4ae6a0880
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-05-25 15:58:11 +0530

    mongo_db_install

--- mongo_db.sh
+++ mongo_db.sh
@@ -1,28 +1,28 @@
-#/bin/bash
+#!/bin/bash
 LOGDIR=/var/log/Mongo
 mkdir -p $LOGDIR
 chown ec2-user:ec2-user $LOGDIR
 chmod 755 $LOGDIR
 LOGFILE=$LOGDIR/$0.log
 R="\e[31m"
-G="\e[32"
-Y="\e[33"
-N="\e[0"
-TIME_STAMP=$(date "+%Y-%m-%d %H:%M:%D")
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIME_STAMP=$(date "+%Y-%m-%d %H:%M:%S")
 USERID=$(id -u)
 
-if [ USERID -eq 0 ]; then
-  echo -e "$TIME_STAMP [ERROR] $R swithch to root user $N" | tee -a $LOGFILE
+if [ $USERID -eq 0 ]; then
+  echo -e "$TIME_STAMP [ERROR] $R switch to root user $N" | tee -a $LOGFILE
   exit 1
 fi
-VALIDATE()
- if [ $1 -nq 0 ]; then
+VALIDATE() {
+ if [ $1 -ne 0 ]; then
  echo -e "$TIME_STAMP [ERROR] $R failed $2 .. $N" | tee -a $LOGFILE
  exit 1
  else
  echo -e "$TIME_STAMP [SUCCESS] $G Success .. $N" | tee -a $LOGFILE
  fi
-
+}
  cp mongo.repo /etc/yum.repos.d/mongo.repo
  VALIDATE $? "Adding mongo.repo"
 
@@ -30,12 +30,12 @@ VALIDATE()
  VALIDATE $? "Installing Mongo"
 
  systemctl enable --now mongodb
- VALIDATE $? "started and enable mongodb"
+ VALIDATE $? "started and enable mongod"
 
- sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf/g
+ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf/
  VALIDATE $? "ip config change"
 
- systemctl restart mongodb
+ systemctl restart mongod
  VALIDATE $? "mongodb restart"
 
 
