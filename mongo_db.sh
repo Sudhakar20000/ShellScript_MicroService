07386f14df4d62c34f0a51cf1ff89657825a8330
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-05-25 16:37:44 +0530

    mongo_db_install

--- mongo_db.sh
+++ mongo_db.sh
@@ -1,44 +1,40 @@
 #!/bin/bash
-LOGDIR=/var/log/Mongo
+LOGDIR=/var/log/mongo_log
 mkdir -p $LOGDIR
-chown ec2-user:ec2-user $LOGDIR
-chmod 755 $LOGDIR
+chmod -R 755  $LOGDIR
+chown -R ec2-user:ec2-user $LOGDIR
 LOGFILE=$LOGDIR/$0.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-TIME_STAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="e\[31m"
+G="e\[32m"
+Y="e\[33m"
+N="e\[0m"
+TIME_STAMP=$(date '+%Y-%m-%d %H:%M:%S')
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
   echo -e "$TIME_STAMP [ERROR] $R switch to root user $N" | tee -a $LOGFILE
   exit 1
 fi
-VALIDATE() {
- if [ $1 -ne 0 ]; then
- echo -e "$TIME_STAMP [ERROR] $R failed $2 .. $N" | tee -a $LOGFILE
- exit 1
- else
- echo -e "$TIME_STAMP [SUCCESS] $G Success $2 .. $N" | tee -a $LOGFILE
- fi
+VALIDATE () {
+  if [ $1 -ne 0 ]; then
+  echo -e "$TIME_STAMP [ERROR] $R error for ..$2 $N" | tee -a $LOGFILE
+  exit 1
+  else
+  echo -e "$TIME_STAMP [SUCCESS] $G success for ..$2 $N" | tee -a $LOGFILE
+  fi
 }
- cp -R mongo.repo /etc/yum.repos.d/
- VALIDATE $? "Adding mongo.repo"
 
- dnf install mongodb-org -y &>> $LOGFILE
- VALIDATE $? "Installing Mongo"
+cp -r mongo.repo /etc/yum.repos.d/
+VALIDATE $? "add repo"
 
- systemctl enable --now mongod
- VALIDATE $? "started and enable mongod"
+dnf install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "install mongodb"
 
- sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
- VALIDATE $? "ip config change"
+systemctl enable --now mongod
+VALIDATE $? "enable and start"
 
- systemctl restart mongod
- VALIDATE $? "mongodb restart"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "change config"
 
-
-
-
-
+syatemctl restart mongod
+VALIDATE $? "restart mongod"
